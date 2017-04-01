242e9d17543d8cee3004d76b91bc8e77fa81b6c1
ellatang <ellatang20170101@gmail.com>
2017-04-01 21:28:06 +0800

    add validation to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,10 +16,13 @@ end
   end
 
   def create
-    @group = Group.new(group_params)
-    @group.save
+   @group = Group.new(group_params)
 
-      redirect_to groups_path
+   if @group.save
+     redirect_to groups_path
+   else
+     render :new
+   end
   end
 
      def update
@@ -37,7 +40,7 @@ end
           redirect_to groups_path
         end
 
-     
+
   private
 
   def group_params
